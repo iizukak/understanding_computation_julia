cd6398828a2c95cb921aff5a6c8322a03a5665c6
Kentaro Iizuka <iizuka.kentaro@gmail.com>
2014-10-11 01:14:12 +0900

    define reduce method

--- src/simple.jl
+++ src/simple.jl
@@ -1,18 +1,40 @@
 # ex. Num(10).value
 # ex. Add(Num(10), Num(2))
 
-type Num
+immutable Num
     value::Number
 end
 
 type Add
-    left::Num
-    right::Num
+    left
+    right
 end
 
 type Mult
-    left::Num
-    right::Num
+    left
+    right
 end
 
+is_reducible(expression::Union(Mult, Add)) = true
+is_reducible(Num) = false
+
+function reduce(exp::Add)
+    if is_reducible(exp.left)
+        return reduce(Add(reduce(exp.left), exp.right))
+    elseif is_reducible(exp.right)
+        return reduce(Add(exp.left, reduce(exp.right)))
+    else
+        return Num(exp.left.value + exp.right.value)
+    end
+end
+
+function reduce(exp::Mult)
+    if is_reducible(exp.left)
+        return reduce(Mult(reduce(exp.left), exp.right))
+    elseif is_reducible(exp.right)
+        return reduce(Mult(exp.left, reduce(exp.right)))
+    else
+        return Num(exp.left.value * exp.right.value)
+    end
+end
 
